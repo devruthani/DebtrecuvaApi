1016cfeef1df2f0aa053214993a284117bee3b8c
devruthani <devruthani@gmail.com>
2025-02-25 10:04:47 +0100

    bug fix on stat

--- src/controller/agents/fieldAgentController.js
+++ src/controller/agents/fieldAgentController.js
@@ -608,7 +608,7 @@ const fieldAgentController = {
     
         var allCustomers = await Assignagent.findAll({where: {agentid}})
         var allCallLogs = await Calllog.findAll({where:{agentid}})
-        var allCallLogs = await Calllog.findAll({where:{clientid}})
+       
         return res.status(200).json({
           error:false,
           message:"Stats acquired Successful",
